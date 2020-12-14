26a52b160f7a4630195d69c6704f0b514aaebcbc
27293 <myltd6@fpt.com.vn>
2020-12-14 14:09:54 +0700

    first commit

--- WebViewInSwiftUI/ContentView.swift
+++ WebViewInSwiftUI/ContentView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            SwiftUIWebView(url: URL(string: "https://www.apple.com/"))
+                .navigationTitle("WebView")
+        }
     }
 }
 
