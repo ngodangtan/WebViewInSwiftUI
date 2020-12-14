919411853691a46f7fc216bfb2b36091dabf1130
27293 <myltd6@fpt.com.vn>
2020-12-14 13:51:15 +0700

    Initial Commit

---
+++ WebViewInSwiftUI/WebViewInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewInSwiftUIApp.swift
+//  WebViewInSwiftUI
+//
+//  Created by Ngo Dang tan on 14/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct WebViewInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
